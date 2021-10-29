8b19c3ce7e7b0bf6c915739d27807214826a60dc
FelixMedicusf <felixbieleit@gmail.com>
2021-10-29 14:44:39 +0200

    This is a commit

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, StatusBar} from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+
 
 export default function App() {
-  console.log("App executed!")
-
-  return (
-    <View style={styles.container}>
-      <Text>Heeloooooo!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return <WelcomeScreen/>
+     
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import {ImageBackground, StyleSheet, View, Image, Text, Button, Alert} from 'react-native';
+
+function WelcomeScreen() {
+    // Declare a new state variable, which we'll call "count" and set it to 0 
+    // State variables are saved even when function is exited
+    // useState() returns the current state and a function that updates it 
+    const [count, setCount] = useState(0);
+    return (
+       <ImageBackground 
+       style={styles.background}
+       source={require('../assets/background.jpg')}
+       >
+           <Button 
+            style={styles.timerButton}
+            onPress={
+                ()=>Alert.alert("Proceed to set timer!")
+            }
+            title="Set Timer"
+           />
+       </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    background:{
+        flex:1,
+        justifyContent:"flex-end", 
+        justifyContent:"center"
+    },
+    timerButton:{
+        color:"navy"
+    },
+    
+
+})
+
+export default WelcomeScreen;
